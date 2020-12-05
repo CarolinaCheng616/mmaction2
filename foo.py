0e9a71ddcb738d0b5c73b719dad804c99f59ccf6
CarolinaCheng616 <948005247@qq.com>
2020-12-05 22:36:44 +0800

    test

--- foo.py
+++ foo.py
@@ -1,10 +1,24 @@
-# from mmcv.runner import _load_checkpoint
-import torch
-import sys
+import copy
+
+
+def foo_save_module():
+    import sys
+    import torch
+    ckpt_path = sys.argv[1]
+    original_ckpt = torch.load(ckpt_path, map_location=torch.device('cpu'))
+    target_ckpt = copy.deepcopy(original_ckpt)
+
+    for key in original_ckpt['state_dict']:
+        new_key = key.replace('module.', '')
+        if 'fc' in new_key:
+            new_key = 'cls_head.' + new_key
+        else:
+            new_key = 'backbone.' + new_key
+        target_ckpt['state_dict'].pop(key)
+        target_ckpt['state_dict'][new_key] = original_ckpt['state_dict'][key]
+
+    torch.save(target_ckpt, sys.argv[2])
 
 
 if __name__ == '__main__':
-    pretrained = sys.argv[1]
-    weights = torch.load(pretrained, map_location='cpu')
-    import pdb
-    pdb.set_trace()
+    foo_save_module()
