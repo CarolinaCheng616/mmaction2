9877514694a58faafc599131377ff7ea0a54031c
CarolinaCheng616 <948005247@qq.com>
2020-12-06 13:27:02 +0800

    test

--- configs/recognition/p3d/p3d_1_r152_kinetics400_trained_rgb.py
+++ configs/recognition/p3d/p3d_1_r152_kinetics400_trained_rgb.py
@@ -64,7 +64,7 @@ test_pipeline = [
     dict(type='ToTensor', keys=['imgs'])
 ]
 data = dict(
-    videos_per_gpu=1,
+    videos_per_gpu=16,
     workers_per_gpu=4,
     train=dict(
         type=dataset_type,
