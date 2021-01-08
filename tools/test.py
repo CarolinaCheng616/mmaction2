04c8e9e0957c535281d2b57807959650fca2d1af
CarolinaCheng616 <948005247@qq.com>
2021-01-08 22:11:57 +0800

    test

--- tools/test.py
+++ tools/test.py
@@ -130,11 +130,12 @@ def main():
             # make sure the dirname of the output path exists
             mmcv.mkdir_or_exist(osp.dirname(out))
             _, suffix = osp.splitext(out)
+            suffix = suffix[1:]
         if dataset_type == 'AVADataset':
-            assert suffix[1:] == 'csv', ('For AVADataset, the format of the '
-                                         'output file should be csv')
+            assert suffix == 'csv', ('For AVADataset, the format of the '
+                                     'output file should be csv')
         else:
-            assert suffix[1:] in file_handlers, (
+            assert suffix in file_handlers, (
                 'The format of the output '
                 'file should be json, pickle or yaml')
 
