b508a6a4515b1f178d8b41f4e2dcf8c10ec42790
CarolinaCheng616 <948005247@qq.com>
2021-01-22 16:44:23 +0800

    snippet

--- configs/localization/bsn/bsn_tem_snippet.py
+++ configs/localization/bsn/bsn_tem_snippet.py
@@ -81,7 +81,7 @@ val_pipeline = [
 
 data = dict(
     videos_per_gpu=128,
-    workers_per_gpu=8,
+    workers_per_gpu=1,
     train_dataloader=dict(drop_last=False),
     val_dataloader=dict(videos_per_gpu=128, workers_per_gpu=4),
     test_dataloader=dict(videos_per_gpu=128, workers_per_gpu=4),
