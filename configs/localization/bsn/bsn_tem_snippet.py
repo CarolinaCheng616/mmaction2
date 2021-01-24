23f728c083fbff96c64a7cc896949adaa4222aef
CarolinaCheng616 <948005247@qq.com>
2021-01-24 13:31:08 +0800

    snippet

--- configs/localization/bsn/bsn_tem_snippet.py
+++ configs/localization/bsn/bsn_tem_snippet.py
@@ -105,14 +105,14 @@ data = dict(
 
 # optimizer
 optimizer = dict(
-    type='SGD', lr=0.001, momentum=0.9, weight_decay=0.0005)  # batch_size
+    type='SGD', lr=0.0001, momentum=0.9, weight_decay=0.0005)  # batch_size
 
 optimizer_config = dict(grad_clip=None)
 # learning policy
 lr_config = dict(policy='step', step=25)
 
 total_epochs = 70
-checkpoint_config = dict(interval=10, filename_tmpl='tem_epoch_{}.pth')
+checkpoint_config = dict(interval=1, filename_tmpl='tem_epoch_{}.pth')
 
 log_config = dict(
     interval=2,
