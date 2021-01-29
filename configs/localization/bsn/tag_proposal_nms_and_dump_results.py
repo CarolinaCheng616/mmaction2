1f796db29f0b52cda76833beb4bb69773447b5d5
CarolinaCheng616 <948005247@qq.com>
2021-01-29 14:07:35 +0800

    snippet

--- configs/localization/bsn/tag_proposal_nms_and_dump_results.py
+++ configs/localization/bsn/tag_proposal_nms_and_dump_results.py
@@ -9,7 +9,9 @@ ann_file_test = 'data/TruNet/val_meta.json'
 pgm_work_dir = 'work_dirs/tag_pgm_snippet'
 pgm_proposals_dir = f'{pgm_work_dir}/pgm_proposals/'
 tem_results_dir = 'work_dirs/tem_snippet/tem_results/'
-tag_proposal_nms_dir = 'work_dirs/tag_pgm_nms_snippet'
+tag_pgm_score_nms_dir = 'work_dirs/tag_pgm_score_nms_snippet'
+tag_pgm_iou_nms_dir = 'work_dirs/tag_pgm_iou_nms_snippet'
+
 topk = 100
 threads = 8
 
@@ -19,21 +21,20 @@ tag_score_nms_config = dict(
     soft_nms_low_threshold=0.65,
     soft_nms_high_threshold=0.9,
     top_k=topk,
-    proposals_dir=f'{tag_proposal_nms_dir}/score_pgm_proposals/',
-    features_dir=f'{tag_proposal_nms_dir}/score_pgm_features/',
+    proposals_dir=f'{tag_pgm_score_nms_dir}/pgm_proposals/',
+    features_dir=f'{tag_pgm_score_nms_dir}/pgm_features/',
     output_config=dict(
-        out=f'{tag_proposal_nms_dir}/score_results.json',
-        output_format='json'))
+        out=f'{tag_pgm_score_nms_dir}/results.json', output_format='json'))
 tag_iou_nms_config = dict(
     thread_num=threads,
     soft_nms_alpha=0.75,
     soft_nms_low_threshold=0.65,
     soft_nms_high_threshold=0.9,
     top_k=topk,
-    proposals_dir=f'{tag_proposal_nms_dir}/iou_pgm_proposals/',
-    features_dir=f'{tag_proposal_nms_dir}/iou_pgm_features/',
+    proposals_dir=f'{tag_pgm_iou_nms_dir}/pgm_proposals/',
+    features_dir=f'{tag_pgm_iou_nms_dir}/pgm_features/',
     output_config=dict(
-        out=f'{tag_proposal_nms_dir}/iou_results.json', output_format='json'))
+        out=f'{tag_pgm_iou_nms_dir}/results.json', output_format='json'))
 feature_kwargs = dict(
     top_k=topk,
     bsp_boundary_ratio=0.2,
