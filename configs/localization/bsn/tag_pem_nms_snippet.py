b185a615decb83a3e11f3bdc38f364218a3be742
CarolinaCheng616 <948005247@qq.com>
2021-01-30 15:55:29 +0800

    snippet

--- configs/localization/bsn/tag_pem_nms_snippet.py
+++ configs/localization/bsn/tag_pem_nms_snippet.py
@@ -33,7 +33,7 @@ ann_file_test = 'data/TruNet/val_meta.json'
 # for iou nms
 nms_type = 'iou'
 
-lr = 1
+lr = 10
 # for train
 # pgm_work_dir = f'work_dirs/tag_pgm_{nms_type}_nms_snippet/'
 # work_dir = f'work_dirs/tag_pem_{nms_type}_nms_snippet_lr{lr}/'
