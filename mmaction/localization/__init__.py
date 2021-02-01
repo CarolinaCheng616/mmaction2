69f7b2ee2481addd37d81bc2b87353d74d4fc254
CarolinaCheng616 <948005247@qq.com>
2021-02-01 22:17:19 +0800

    snippet

--- mmaction/localization/__init__.py
+++ mmaction/localization/__init__.py
@@ -2,12 +2,10 @@ from .bsn_utils import generate_bsp_feature, generate_candidate_proposals
 from .proposal_utils import soft_nms, temporal_iop, temporal_iou
 from .ssn_utils import (eval_ap, load_localize_proposal_file,
                         perform_regression, temporal_nms)
-from .tag_utils import (generate_tag_feature, generate_tag_proposals,
-                        nms_and_dump_results, proposals_post_processing)
+from .tag_utils import nms_and_dump_results
 
 __all__ = [
     'generate_candidate_proposals', 'generate_bsp_feature', 'temporal_iop',
     'temporal_iou', 'soft_nms', 'load_localize_proposal_file',
-    'perform_regression', 'temporal_nms', 'eval_ap', 'generate_tag_proposals',
-    'generate_tag_feature', 'proposals_post_processing', 'nms_and_dump_results'
+    'perform_regression', 'temporal_nms', 'eval_ap', 'nms_and_dump_results'
 ]
