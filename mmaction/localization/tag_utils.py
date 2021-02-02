7dad9025227171dfe8b22affc158f6cab705eba2
CarolinaCheng616 <948005247@qq.com>
2021-02-02 14:26:08 +0800

    snippet

--- mmaction/localization/tag_utils.py
+++ mmaction/localization/tag_utils.py
@@ -570,9 +570,6 @@ def _generate_tag_original_feature(video_list,
         for i in range(video_scale):
             middle_tp.append(video_gap / 2 + i * video_gap)
         t_points = begin_tp + middle_tp + end_tp
-        print(score_action.shape)
-        print(len(t_points))
-        print('\n')
 
         bsp_feature = []
         f = interpolate.interp1d(t_points, score_action, axis=0)
@@ -598,12 +595,11 @@ def _generate_tag_original_feature(video_list,
             ]
             # print(f't_new.len={len(t_new)}')
             y_new_start_action = f(t_new)
-            print(f'y_new_start_action.shape={y_new_start_action.shape}')
-            exit(0)
             y_new_start = [
-                np.mean(y_new_start_action[i * num_sample_interp:(i + 1) *
-                                           num_sample_interp + 1])
-                for i in range(num_sample_start)
+                np.mean(
+                    y_new_start_action[i * num_sample_interp:(i + 1) *
+                                       num_sample_interp + 1],
+                    axis=0) for i in range(num_sample_start)
             ]
             # print(f'y_new_start.shape = {len(y_new_start)}')
             # Generate features at end boundary
