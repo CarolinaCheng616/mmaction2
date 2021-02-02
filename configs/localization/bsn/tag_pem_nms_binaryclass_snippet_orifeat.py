48c10aeabc2b044e110d14a4562fb5010145375e
CarolinaCheng616 <948005247@qq.com>
2021-02-02 14:05:51 +0800

    snippet

--- configs/localization/bsn/tag_pem_nms_binaryclass_snippet_orifeat.py
+++ configs/localization/bsn/tag_pem_nms_binaryclass_snippet_orifeat.py
@@ -32,7 +32,7 @@ ann_file_train = 'data/TruNet/train_meta.json'
 ann_file_val = 'data/TruNet/val_meta.json'
 ann_file_test = 'data/TruNet/val_meta.json'
 
-proposal_topk = 100
+proposal_topk = 500
 lr = 0.1
 if model['loss_cls']['type'] == 'BinaryLogisticRegressionLoss':
     loss_cls = 'blr'
@@ -47,16 +47,18 @@ else:
 nms_type = 'iou'
 
 # for train
-pgm_work_dir = f'work_dirs/tag_pgm_{nms_type}_nms_snippet_{proposal_topk}/'
+pgm_work_dir = f'work_dirs/' \
+               f'tag_pgm_{nms_type}_nms_snippet_orifeat_{proposal_topk}/'
 work_dir = f'work_dirs/tag_pem_{nms_type}_nms_' \
-           f'{proposal_topk}_{loss_cls}_snippet_lr{lr}/'
+           f'{proposal_topk}_{loss_cls}_snippet_orifeat_lr{lr}/'
 pgm_proposals_dir = f'{pgm_work_dir}/pgm_proposals/'
 pgm_features_dir = f'{pgm_work_dir}/pgm_features/'
 
 # for test
-# pgm_work_dir = 'work_dirs/tag_pgm_snippet/'
+# pgm_work_dir = f'work_dirs/' \
+#                f'tag_pgm_{nms_type}_nms_snippet_orifeat_{proposal_topk}/'
 # work_dir = f'work_dirs/tag_pem_{nms_type}_nms_' \
-#            f'{proposal_topk}_{loss_cls}_snippet_lr{lr}/'
+#            f'{proposal_topk}_{loss_cls}_snippet_orifeat_lr{lr}/'
 # pgm_proposals_dir = f'{pgm_work_dir}/pgm_proposals/'
 # pgm_features_dir = f'{pgm_work_dir}/pgm_features/'
 
