b78a25820564a03824a3d1cf83221c7f24ae228a
CarolinaCheng616 <948005247@qq.com>
2021-02-02 16:59:02 +0800

    snippet

--- configs/localization/bsn/tag_pem_nms_binaryclass_snippet_orifeat.py
+++ configs/localization/bsn/tag_pem_nms_binaryclass_snippet_orifeat.py
@@ -55,8 +55,7 @@ nms_type = 'iou'
 # pgm_features_dir = f'{pgm_work_dir}/pgm_features/'
 
 # for test
-pgm_work_dir = f'work_dirs/' \
-               f'tag_pgm_{nms_type}_nms_snippet_orifeat_{proposal_topk}/'
+pgm_work_dir = 'work_dirs/tag_pgm_snippet/'
 work_dir = f'work_dirs/tag_pem_{nms_type}_nms_' \
            f'{proposal_topk}_{loss_cls}_snippet_orifeat_lr{lr}/'
 pgm_proposals_dir = f'{pgm_work_dir}/pgm_proposals/'
