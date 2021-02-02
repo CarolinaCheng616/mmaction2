c38a07fb31cbb5ca4a47833ae3ef5a40e62eccdc
CarolinaCheng616 <948005247@qq.com>
2021-02-02 13:38:17 +0800

    snippet

--- mmaction/models/localizers/__init__.py
+++ mmaction/models/localizers/__init__.py
@@ -1,10 +1,10 @@
 from .base import BaseLocalizer
 from .bmn import BMN
-from .bsn import _TEM_, PEM, TAG_PEM, TEM, ClassifyPEM, fcTEM
+from .bsn import _TEM_, PEM, TAG_PEM, TEM, ClassifyPEM, OriFeatPEM, fcTEM
 from .snippetwise_bsn import SnippetTEM
 from .ssn import SSN
 
 __all__ = [
     'PEM', 'TEM', 'fcTEM', 'BMN', 'SSN', 'BaseLocalizer', '_TEM_',
-    'SnippetTEM', 'TAG_PEM', 'ClassifyPEM'
+    'SnippetTEM', 'TAG_PEM', 'ClassifyPEM', 'OriFeatPEM'
 ]
