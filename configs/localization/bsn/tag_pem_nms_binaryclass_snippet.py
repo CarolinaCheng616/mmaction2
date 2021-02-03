d0d8ea2f657fd433bd3cd82402c6ed958443bdeb
CarolinaCheng616 <948005247@qq.com>
2021-02-03 10:38:05 +0800

    snippet

--- configs/localization/bsn/tag_pem_nms_binaryclass_snippet.py
+++ configs/localization/bsn/tag_pem_nms_binaryclass_snippet.py
@@ -18,7 +18,7 @@ model = dict(
     post_process_top_k=100,
     fc1_ratio=1,
     fc2_ratio=1,
-    loss_cls=dict(type='BinaryThresholdClassificationLoss'))
+    loss_cls=dict(type='BinaryLogisticRegressionLoss'))
 # model training and testing settings
 train_cfg = None
 test_cfg = dict(average_clips='score')
@@ -46,21 +46,20 @@ else:
 nms_type = 'iou'
 
 # for train
-# pgm_work_dir = f'work_dirs/tag_pgm_{nms_type}_nms_snippet_{proposal_topk}/'
-# work_dir = f'work_dirs/tag_pem_{nms_type}_nms_' \
-#            f'{proposal_topk}_{loss_cls}_snippet_lr{lr}/'
-# pgm_proposals_dir = f'{pgm_work_dir}/pgm_proposals/'
-# pgm_features_dir = f'{pgm_work_dir}/pgm_features/'
-
-# for test
-pgm_work_dir = 'work_dirs/tag_pgm_snippet/'
+pgm_work_dir = f'work_dirs/tag_pgm_{nms_type}_nms_snippet_{proposal_topk}/'
 work_dir = f'work_dirs/tag_pem_{nms_type}_nms_' \
            f'{proposal_topk}_{loss_cls}_snippet_lr{lr}/'
 pgm_proposals_dir = f'{pgm_work_dir}/pgm_proposals/'
 pgm_features_dir = f'{pgm_work_dir}/pgm_features/'
 
-output_config = dict(
-    out=f'{work_dir}/epoch_70_results.json', output_format='json')
+# for test
+# pgm_work_dir = 'work_dirs/tag_pgm_snippet/'
+# work_dir = f'work_dirs/tag_pem_{nms_type}_nms_' \
+#            f'{proposal_topk}_{loss_cls}_snippet_lr{lr}/'
+# pgm_proposals_dir = f'{pgm_work_dir}/pgm_proposals/'
+# pgm_features_dir = f'{pgm_work_dir}/pgm_features/'
+
+output_config = dict(out=f'{work_dir}/results.json', output_format='json')
 
 test_pipeline = [
     dict(
