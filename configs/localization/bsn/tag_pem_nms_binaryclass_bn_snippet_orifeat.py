2186678dc71a5e55c0649d9881a65d90fe4e39df
CarolinaCheng616 <948005247@qq.com>
2021-02-04 14:00:06 +0800

    snippet

--- configs/localization/bsn/tag_pem_nms_binaryclass_bn_snippet_orifeat.py
+++ configs/localization/bsn/tag_pem_nms_binaryclass_bn_snippet_orifeat.py
@@ -112,7 +112,7 @@ val_pipeline = [
 ]
 data = dict(
     videos_per_gpu=16,
-    workers_per_gpu=8,
+    workers_per_gpu=0,
     train_dataloader=dict(drop_last=False),
     val_dataloader=dict(videos_per_gpu=1),
     test_dataloader=dict(videos_per_gpu=1),
