2f0fdcb20918b4e13166c1bd27ffc358c7a60196
CarolinaCheng616 <948005247@qq.com>
2021-02-07 20:26:43 +0800

    snippet

--- configs/localization/bsn/tag_pem_nms_binaryclass_bn_snippet_offset.py
+++ configs/localization/bsn/tag_pem_nms_binaryclass_bn_snippet_offset.py
@@ -142,7 +142,7 @@ data = dict(
 # optimizer
 # optimizer = dict(
 #     type='Adam', lr=0.01, weight_decay=0.00001)  # this lr is used for 1 gpus
-gpu_per_node = 2
+gpu_per_node = 4
 machines = 1
 optimizer = dict(
     type='SGD',
