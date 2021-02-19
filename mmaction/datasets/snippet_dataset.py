9704be7c92fc9f4bbdc895d022fe4a082f527265
CarolinaCheng616 <948005247@qq.com>
2021-02-19 16:23:31 +0800

    snippet

--- mmaction/datasets/snippet_dataset.py
+++ mmaction/datasets/snippet_dataset.py
@@ -239,7 +239,6 @@ class SnippetSRDataset(TruNetDataset):   # snippet sample ratio dataset
 
     def sample_ratio(self):
         """Control sampling ratio."""
-        # boundary_snippets = self.boundary_snippets * self.duplicate_number
         start_snippets = self.start_snippets * self.duplicate_number
         end_snippets = self.end_snippets * self.duplicate_number
         start_number = len(start_snippets)
@@ -254,6 +253,8 @@ class SnippetSRDataset(TruNetDataset):   # snippet sample ratio dataset
         self.filtered_snippet_infos = sorted(
             self.filtered_snippet_infos,
             key=lambda x: '_'.join(x['video_name'].split('_')[:-1]))
+        import pdb
+        pdb.set_trace()
 
     def dump_results(self, results, out, output_format, version='VERSION 1.3'):
         if output_format == 'json':
