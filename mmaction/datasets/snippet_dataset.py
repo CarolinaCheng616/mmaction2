fa92070c51c5b3bde9deb8c5ab385405c216f5fb
CarolinaCheng616 <948005247@qq.com>
2021-02-19 14:15:30 +0800

    snippet

--- mmaction/datasets/snippet_dataset.py
+++ mmaction/datasets/snippet_dataset.py
@@ -248,8 +248,6 @@ class SnippetSRDataset(TruNetDataset):   # snippet sample ratio dataset
         self.filtered_snippet_infos = sorted(
             self.filtered_snippet_infos,
             key=lambda x: '_'.join(x['video_name'].split('_')[:-1]))
-        import pdb
-        pdb.set_trace()
 
     def dump_results(self, results, out, output_format, version='VERSION 1.3'):
         """Dump data to json/csv files."""
