f35216d8d35f1da4c29665ecbedae2bc97c82322
CarolinaCheng616 <948005247@qq.com>
2021-02-24 22:03:49 +0800

    snippet

--- mmaction/models/localizers/ff_lstm.py
+++ mmaction/models/localizers/ff_lstm.py
@@ -21,13 +21,15 @@ class FFLSTM(nn.Module):
     def forward(self, x):
         # x.shape: [seq_len, batch, feature_size]
         _, batch, _ = x.shape
-        # device = x.device
+        device = x.device
         if self.with_init_hc:
             ht = torch.randn(self.num_layers, batch, self.hidden_size)
             ct = torch.randn(self.num_layers, batch, self.hidden_size)
         else:
             ht = torch.zeros(self.num_layers, batch, self.hidden_size)
             ct = torch.zeros(self.num_layers, batch, self.hidden_size)
+        ht = ht.to(device, type=torch.float32)
+        ct = ct.to(device, type=torch.float32)
         output = []  # seq_len, batch, hidden_size
         for i in range(self.seq_len):
             ht[0], ct[0] = self.cells[0](x[i], (ht[0], ct[0]))
