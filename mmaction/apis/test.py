58e7c851dd98e64afe27fcb36fdb3f90f5864bf8
CarolinaCheng616 <948005247@qq.com>
2021-04-01 12:32:03 +0800

    bert

--- mmaction/apis/test.py
+++ mmaction/apis/test.py
@@ -61,6 +61,9 @@ def multi_gpu_test(model, data_loader, tmpdir=None, gpu_collect=True):
     results = []
     dataset = data_loader.dataset
     rank, world_size = get_dist_info()
+    import pdb
+
+    pdb.set_trace()
     if rank == 0:
         prog_bar = mmcv.ProgressBar(len(dataset))
     for data in data_loader:
@@ -102,16 +105,14 @@ def collect_results_cpu(result_part, size, tmpdir=None):
     if tmpdir is None:
         MAX_LEN = 512
         # 32 is whitespace
-        dir_tensor = torch.full((MAX_LEN, ),
-                                32,
-                                dtype=torch.uint8,
-                                device='cuda')
+        dir_tensor = torch.full((MAX_LEN,), 32, dtype=torch.uint8, device="cuda")
         if rank == 0:
-            mmcv.mkdir_or_exist('.dist_test')
-            tmpdir = tempfile.mkdtemp(dir='.dist_test')
+            mmcv.mkdir_or_exist(".dist_test")
+            tmpdir = tempfile.mkdtemp(dir=".dist_test")
             tmpdir = torch.tensor(
-                bytearray(tmpdir.encode()), dtype=torch.uint8, device='cuda')
-            dir_tensor[:len(tmpdir)] = tmpdir
+                bytearray(tmpdir.encode()), dtype=torch.uint8, device="cuda"
+            )
+            dir_tensor[: len(tmpdir)] = tmpdir
         dist.broadcast(dir_tensor, 0)
         tmpdir = dir_tensor.cpu().numpy().tobytes().decode().rstrip()
     else:
@@ -119,7 +120,7 @@ def collect_results_cpu(result_part, size, tmpdir=None):
     # synchronizes all processes to make sure tmpdir exist
     dist.barrier()
     # dump the part result to the dir
-    mmcv.dump(result_part, osp.join(tmpdir, f'part_{rank}.pkl'))
+    mmcv.dump(result_part, osp.join(tmpdir, f"part_{rank}.pkl"))
     # synchronizes all processes for loding pickle file
     dist.barrier()
     # collect all parts
@@ -128,7 +129,7 @@ def collect_results_cpu(result_part, size, tmpdir=None):
     # load results of all parts from tmp dir
     part_list = []
     for i in range(world_size):
-        part_file = osp.join(tmpdir, f'part_{i}.pkl')
+        part_file = osp.join(tmpdir, f"part_{i}.pkl")
         part_list.append(mmcv.load(part_file))
     # sort the results
     ordered_results = []
@@ -157,26 +158,24 @@ def collect_results_gpu(result_part, size):
     rank, world_size = get_dist_info()
     # dump result part to tensor with pickle
     part_tensor = torch.tensor(
-        bytearray(pickle.dumps(result_part)), dtype=torch.uint8, device='cuda')
+        bytearray(pickle.dumps(result_part)), dtype=torch.uint8, device="cuda"
+    )
     # gather all result part tensor shape
-    shape_tensor = torch.tensor(part_tensor.shape, device='cuda')
+    shape_tensor = torch.tensor(part_tensor.shape, device="cuda")
     shape_list = [shape_tensor.clone() for _ in range(world_size)]
     dist.all_gather(shape_list, shape_tensor)
     # padding result part tensor to max length
     shape_max = torch.tensor(shape_list).max()
-    part_send = torch.zeros(shape_max, dtype=torch.uint8, device='cuda')
-    part_send[:shape_tensor[0]] = part_tensor
-    part_recv_list = [
-        part_tensor.new_zeros(shape_max) for _ in range(world_size)
-    ]
+    part_send = torch.zeros(shape_max, dtype=torch.uint8, device="cuda")
+    part_send[: shape_tensor[0]] = part_tensor
+    part_recv_list = [part_tensor.new_zeros(shape_max) for _ in range(world_size)]
     # gather all result part
     dist.all_gather(part_recv_list, part_send)
 
     if rank == 0:
         part_list = []
         for recv, shape in zip(part_recv_list, shape_list):
-            part_list.append(
-                pickle.loads(recv[:shape[0]].cpu().numpy().tobytes()))
+            part_list.append(pickle.loads(recv[: shape[0]].cpu().numpy().tobytes()))
         # sort the results
         ordered_results = []
         for res in zip(*part_list):
