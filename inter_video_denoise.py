bd907356e2fc4eb6fdb1bbb63c0eaba5f174defd
CarolinaCheng616 <948005247@qq.com>
2021-04-08 14:54:49 +0800

    denoise

--- inter_video_denoise.py
+++ inter_video_denoise.py
@@ -292,6 +292,9 @@ class Filter:
                 for dis, weight, in zip(distance_list, self.distance_weight_list)
             ]
         )
+        import pdb
+
+        pdb.set_trace()
         db = DBSCAN(eps=eps, metric="precomputed", min_samples=num_samples).fit(
             distance
         )
