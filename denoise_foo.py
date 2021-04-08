1769705b57df97cb64fa112c4f1cb9147f0ce24f
CarolinaCheng616 <948005247@qq.com>
2021-04-08 16:21:06 +0800

    denoise

--- denoise_foo.py
+++ denoise_foo.py
@@ -260,5 +260,6 @@ if __name__ == "__main__":
     # dm_file = "data/bilibili/dm_files.txt"
     # dm_dup_file = "data/bilibili/dm_duplicated_files.txt"
     # dm_uniq_file = "data/bilibili/dm_uniq_files.txt"
-    file = "/mnt/lustrenew/DATAshare/bilibili/intra_denoise_files.txt"
-    get_feature(file)
+    # file = "/mnt/lustrenew/DATAshare/bilibili/intra_denoise_files.txt"
+    # get_feature(file)
+    test_feature_distance()
