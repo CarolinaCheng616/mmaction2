6dc34d7a81026536d2ab64c0648941e6db41823b
CarolinaCheng616 <948005247@qq.com>
2021-04-12 23:10:20 +0800

    v_t

--- mmaction/models/matchers/__init__.py
+++ mmaction/models/matchers/__init__.py
@@ -1,7 +1,14 @@
 from .base import BaseMatcher
+from .video_audio_text_matcher_e2e import VideoAudioTextMatcherE2E
+from .video_subtitle_audio_text_matcher_e2e import \
+    VideoSubtitleAudioTextMatcherE2E
+from .video_subtitle_text_matcher_e2e import VideoSubtitleTextMatcherE2E
 from .video_text_matcher import VideoTextMatcher
 from .video_text_matcher_e2e import VideoTextMatcherE2E
-from .video_audio_text_matcher_e2e import VideoAudioTextMatcherE2E
-from .video_subtitle_text_matcher_e2e import VideoSubtitleTextMatcherE2E
-from .video_subtitle_audio_text_matcher_e2e import VideoSubtitleAudioTextMatcherE2E
-__all__ = ['BaseMatcher', 'VideoTextMatcher','VideoTextMatcherE2E','VideoAudioTextMatcherE2E','VideoSubtitleTextMatcherE2E','VideoSubtitleAudioTextMatcherE2E']
+from .video_text_matcher_memory_bank_e2e import VideoTextMatcherBankE2E
+
+__all__ = [
+    'BaseMatcher', 'VideoTextMatcher', 'VideoTextMatcherE2E',
+    'VideoAudioTextMatcherE2E', 'VideoSubtitleTextMatcherE2E',
+    'VideoSubtitleAudioTextMatcherE2E', 'VideoTextMatcherBankE2E'
+]
