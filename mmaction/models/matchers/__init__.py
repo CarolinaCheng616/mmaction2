b80f63eb477ef3a09735452e2947bf25a2c49172
CarolinaCheng616 <948005247@qq.com>
2021-04-12 23:10:20 +0800

    v_t

--- mmaction/models/matchers/__init__.py
+++ mmaction/models/matchers/__init__.py
@@ -1,11 +1,17 @@
 from .base import BaseMatcher
+from .video_audio_text_matcher_e2e import VideoAudioTextMatcherE2E
+from .video_subtitle_audio_text_matcher_e2e import \
+    VideoSubtitleAudioTextMatcherE2E
+from .video_subtitle_text_matcher_e2e import VideoSubtitleTextMatcherE2E
 from .video_text_matcher import VideoTextMatcher
 from .video_text_matcher_e2e import VideoTextMatcherE2E
-from .video_audio_text_matcher_e2e import VideoAudioTextMatcherE2E
-from .video_subtitle_text_matcher_e2e import VideoSubtitleTextMatcherE2E
-from .video_subtitle_audio_text_matcher_e2e import VideoSubtitleAudioTextMatcherE2E
+from .video_text_matcher_memory_bank_e2e import VideoTextMatcherBankE2E
 from .video_word2vec_matcher_e2e import VideoWord2VecMatcherE2E
 from .video_text_matcher_nsim_loss import VideoTextMatcherNSimLoss
-__all__ = ['BaseMatcher', 'VideoTextMatcher','VideoTextMatcherE2E','VideoAudioTextMatcherE2E',
-           'VideoSubtitleTextMatcherE2E','VideoSubtitleAudioTextMatcherE2E','VideoWord2VecMatcherE2E',
-           'VideoTextMatcherNSimLoss']
+
+__all__ = [
+    'BaseMatcher', 'VideoTextMatcher', 'VideoTextMatcherE2E',
+    'VideoAudioTextMatcherE2E', 'VideoSubtitleTextMatcherE2E',
+    'VideoSubtitleAudioTextMatcherE2E', 'VideoTextMatcherBankE2E',
+    'VideoWord2VecMatcherE2E', 'VideoTextMatcherNSimLoss',
+]
