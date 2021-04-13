cb8775dc615eecd240809210addb7a8247d1dd76
CarolinaCheng616 <948005247@qq.com>
2021-04-13 12:58:21 +0800

    vt

--- mmaction/models/heads/__init__.py
+++ mmaction/models/heads/__init__.py
@@ -13,9 +13,23 @@ from .contrastive_head import ContrastiveHead
 from .mil_nce_head import MILNCEHead
 from .ranking_head import RankingHead
 from .neg_sim_head import NegSimHead
+from .mb_nce_head import MBNCEHead
 
 __all__ = [
-    'TSNHead', 'I3DHead', 'BaseHead', 'TSMHead', 'SlowFastHead', 'SSNHead',
-    'TPNHead', 'AudioTSNHead', 'X3DHead', 'BBoxHeadAVA', 'AVARoIHead', 'ContrastiveHead', 'MILNCEHead', 'RankingHead',
-    'NegSimHead'
+    "TSNHead",
+    "I3DHead",
+    "BaseHead",
+    "TSMHead",
+    "SlowFastHead",
+    "SSNHead",
+    "TPNHead",
+    "AudioTSNHead",
+    "X3DHead",
+    "BBoxHeadAVA",
+    "AVARoIHead",
+    "ContrastiveHead",
+    "MILNCEHead",
+    "RankingHead",
+    "NegSimHead",
+    "MBNCEHead",
 ]
