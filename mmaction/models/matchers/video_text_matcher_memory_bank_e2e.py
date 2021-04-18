31a23e030582bd9779761e7636f162735ed61cde
CarolinaCheng616 <948005247@qq.com>
2021-04-18 11:37:06 +0800

    v_t

--- mmaction/models/matchers/video_text_matcher_memory_bank_e2e.py
+++ mmaction/models/matchers/video_text_matcher_memory_bank_e2e.py
@@ -139,12 +139,12 @@ class VideoTextMatcherBankE2E(BaseMatcher):
 
     def forward_train(self, imgs, texts_item, idxes):
         # BNCHW
-        # import pdb
-        #
-        # pdb.set_trace()
-        self.iters += 1
-        if self.iters == 12000:
-            pdb.set_trace()
+        import pdb
+
+        pdb.set_trace()
+        # self.iters += 1
+        # if self.iters == 12000:
+        #     pdb.set_trace()
         device = imgs.device
         batch_size = imgs.shape[0]
         imgs = imgs.reshape((-1,) + imgs.shape[2:])  # [N'CHW]
