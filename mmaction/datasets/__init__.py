a7dfadddba8c7cd1a5bdfaa7f591d61cfa1ebc3f
CarolinaCheng616 <948005247@qq.com>
2021-04-19 22:36:16 +0800

    feature_extract

--- mmaction/datasets/__init__.py
+++ mmaction/datasets/__init__.py
@@ -17,11 +17,28 @@ from .video_audio_text_dataset import VideoAudioTextDataset
 from .video_subtitle_text_dataset import VideoSubtitleTextDataset
 from .video_subtitle_audio_text_dataset import VideoSubtitleAudioTextDataset
 from .mp4_text_dataset import Mp4TextDataset
+from .my_dataset import MyDataset
+
 __all__ = [
-    'VideoDataset', 'build_dataloader', 'build_dataset', 'RepeatDataset',
-    'RawframeDataset', 'BaseDataset', 'ActivityNetDataset', 'SSNDataset',
-    'HVUDataset', 'AudioDataset', 'AudioFeatureDataset', 'ImageDataset',
-    'RawVideoDataset', 'AVADataset', 'AudioVisualDataset','VideoTextDataset',
-    'VideoAudioTextDataset', 'VideoSubtitleTextDataset','VideoSubtitleAudioTextDataset',
-    'Mp4TextDataset'
+    "VideoDataset",
+    "build_dataloader",
+    "build_dataset",
+    "RepeatDataset",
+    "RawframeDataset",
+    "BaseDataset",
+    "ActivityNetDataset",
+    "SSNDataset",
+    "HVUDataset",
+    "AudioDataset",
+    "AudioFeatureDataset",
+    "ImageDataset",
+    "RawVideoDataset",
+    "AVADataset",
+    "AudioVisualDataset",
+    "VideoTextDataset",
+    "VideoAudioTextDataset",
+    "VideoSubtitleTextDataset",
+    "VideoSubtitleAudioTextDataset",
+    "Mp4TextDataset",
+    "MyDataset",
 ]
