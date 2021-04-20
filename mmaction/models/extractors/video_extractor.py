9d3684c1eec45001ca1bc0b7d754d82eb668f42f
CarolinaCheng616 <948005247@qq.com>
2021-04-20 10:23:06 +0800

    feature_extract

--- mmaction/models/extractors/video_extractor.py
+++ mmaction/models/extractors/video_extractor.py
@@ -20,20 +20,18 @@ class VideoExtractor(FeatureExtractor):
 
     def forward_test(self, x, img_metas=None):
         # x.shape = BNCHW
-        print(x.shape)
         with torch.no_grad():
             batch, frames = x.shape[:2]
             x = x.reshape((-1,) + x.shape[2:])  # BN * CHW
             x = self.backbone(x)  # BN * 2048 * H' * W'
-            # x = x.reshape(batch, frames, *x.shape[1:])  # B * N * 2048 * H' * W'
-            x = self.avgpool(x)  # B * N * 2048 * 1 * 1
+            x = self.avgpool(x)  # BN * 2048 * 1 * 1
             x = x.reshape(batch, frames, -1)  # B * N * 2048
             for i in range(batch):
                 feature = x[i].unsqueeze(0).cpu().detach().numpy()  # 1, 32, 2048
                 feature_path = img_metas[i]["featurepath"]
-                if feature_path.enswith(".npy"):
+                if feature_path.endswith(".npy"):
                     np.save(feature_path, feature)
-        # sys.exit(0)
+        sys.exit(0)
 
     def forward(self, imgs, return_loss=False, img_metas=None):
         self.forward_test(imgs, img_metas)
