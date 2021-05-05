ecae77c792362b958027a20522e9bf497e235f55
CarolinaCheng616 <948005247@qq.com>
2021-05-05 19:44:59 +0800

    nsim

--- tools/train.py
+++ tools/train.py
@@ -19,49 +19,52 @@ from mmaction.utils import collect_env, get_root_logger
 
 
 def parse_args():
-    parser = argparse.ArgumentParser(description='Train a recognizer')
-    parser.add_argument('config', help='train config file path')
-    parser.add_argument('--work-dir', help='the dir to save logs and models')
+    parser = argparse.ArgumentParser(description="Train a recognizer")
+    parser.add_argument("config", help="train config file path")
+    parser.add_argument("--work-dir", help="the dir to save logs and models")
+    parser.add_argument("--resume-from", help="the checkpoint file to resume from")
     parser.add_argument(
-        '--resume-from', help='the checkpoint file to resume from')
-    parser.add_argument(
-        '--validate',
-        action='store_true',
-        help='whether to evaluate the checkpoint during training')
+        "--validate",
+        action="store_true",
+        help="whether to evaluate the checkpoint during training",
+    )
     group_gpus = parser.add_mutually_exclusive_group()
     group_gpus.add_argument(
-        '--gpus',
+        "--gpus",
         type=int,
-        help='number of gpus to use '
-        '(only applicable to non-distributed training)')
+        help="number of gpus to use " "(only applicable to non-distributed training)",
+    )
     group_gpus.add_argument(
-        '--gpu-ids',
+        "--gpu-ids",
         type=int,
-        nargs='+',
-        help='ids of gpus to use '
-        '(only applicable to non-distributed training)')
-    parser.add_argument('--seed', type=int, default=None, help='random seed')
+        nargs="+",
+        help="ids of gpus to use " "(only applicable to non-distributed training)",
+    )
+    parser.add_argument("--seed", type=int, default=None, help="random seed")
     parser.add_argument(
-        '--deterministic',
-        action='store_true',
-        help='whether to set deterministic options for CUDNN backend.')
+        "--deterministic",
+        action="store_true",
+        help="whether to set deterministic options for CUDNN backend.",
+    )
     parser.add_argument(
-        '--cfg-options',
-        nargs='+',
+        "--cfg-options",
+        nargs="+",
         action=DictAction,
         default={},
-        help='override some settings in the used config, the key-value pair '
-        'in xxx=yyy format will be merged into config file. For example, '
-        "'--cfg-options model.backbone.depth=18 model.backbone.with_cp=True'")
+        help="override some settings in the used config, the key-value pair "
+        "in xxx=yyy format will be merged into config file. For example, "
+        "'--cfg-options model.backbone.depth=18 model.backbone.with_cp=True'",
+    )
     parser.add_argument(
-        '--launcher',
-        choices=['none', 'pytorch', 'slurm', 'mpi'],
-        default='none',
-        help='job launcher')
-    parser.add_argument('--local_rank', type=int, default=0)
+        "--launcher",
+        choices=["none", "pytorch", "slurm", "mpi"],
+        default="none",
+        help="job launcher",
+    )
+    parser.add_argument("--local_rank", type=int, default=0)
     args = parser.parse_args()
-    if 'LOCAL_RANK' not in os.environ:
-        os.environ['LOCAL_RANK'] = str(args.local_rank)
+    if "LOCAL_RANK" not in os.environ:
+        os.environ["LOCAL_RANK"] = str(args.local_rank)
 
     return args
 
@@ -74,7 +77,7 @@ def main():
     cfg.merge_from_dict(args.cfg_options)
 
     # set cudnn_benchmark
-    if cfg.get('cudnn_benchmark', False):
+    if cfg.get("cudnn_benchmark", False):
         torch.backends.cudnn.benchmark = True
 
     # work_dir is determined in this priority:
@@ -82,10 +85,11 @@ def main():
     if args.work_dir is not None:
         # update configs according to CLI args if args.work_dir is not None
         cfg.work_dir = args.work_dir
-    elif cfg.get('work_dir', None) is None:
+    elif cfg.get("work_dir", None) is None:
         # use config filename as default work_dir if cfg.work_dir is None
-        cfg.work_dir = osp.join('./work_dirs',
-                                osp.splitext(osp.basename(args.config))[0])
+        cfg.work_dir = osp.join(
+            "./work_dirs", osp.splitext(osp.basename(args.config))[0]
+        )
     if args.resume_from is not None:
         cfg.resume_from = args.resume_from
     if args.gpu_ids is not None:
@@ -94,22 +98,22 @@ def main():
         cfg.gpu_ids = range(1) if args.gpus is None else range(args.gpus)
 
     # init distributed env first, since logger depends on the dist info.
-    if args.launcher == 'none':
+    if args.launcher == "none":
         distributed = False
     else:
         distributed = True
         init_dist(args.launcher, **cfg.dist_params)
 
     # The flag is used to determine whether it is omnisource training
-    cfg.setdefault('omnisource', False)
+    cfg.setdefault("omnisource", False)
 
     # create work_dir
     mmcv.mkdir_or_exist(osp.abspath(cfg.work_dir))
     # dump config
     cfg.dump(osp.join(cfg.work_dir, osp.basename(args.config)))
     # init logger before other steps
-    timestamp = time.strftime('%Y%m%d_%H%M%S', time.localtime())
-    log_file = osp.join(cfg.work_dir, f'{timestamp}.log')
+    timestamp = time.strftime("%Y%m%d_%H%M%S", time.localtime())
+    log_file = osp.join(cfg.work_dir, f"{timestamp}.log")
     logger = get_root_logger(log_file=log_file, log_level=cfg.log_level)
 
     # init the meta dict to record some important information such as
@@ -117,28 +121,27 @@ def main():
     meta = dict()
     # log env info
     env_info_dict = collect_env()
-    env_info = '\n'.join([f'{k}: {v}' for k, v in env_info_dict.items()])
-    dash_line = '-' * 60 + '\n'
-    logger.info('Environment info:\n' + dash_line + env_info + '\n' +
-                dash_line)
-    meta['env_info'] = env_info
+    env_info = "\n".join([f"{k}: {v}" for k, v in env_info_dict.items()])
+    dash_line = "-" * 60 + "\n"
+    logger.info("Environment info:\n" + dash_line + env_info + "\n" + dash_line)
+    meta["env_info"] = env_info
 
     # log some basic info
-    logger.info(f'Distributed training: {distributed}')
-    logger.info(f'Config: {cfg.text}')
+    logger.info(f"Distributed training: {distributed}")
+    logger.info(f"Config: {cfg.pretty_text}")
 
     # set random seeds
     if args.seed is not None:
-        logger.info(f'Set random seed to {args.seed}, '
-                    f'deterministic: {args.deterministic}')
+        logger.info(
+            f"Set random seed to {args.seed}, " f"deterministic: {args.deterministic}"
+        )
         set_random_seed(args.seed, deterministic=args.deterministic)
     cfg.seed = args.seed
-    meta['seed'] = args.seed
-    meta['config_name'] = osp.basename(args.config)
-    meta['work_dir'] = osp.basename(cfg.work_dir.rstrip('/\\'))
+    meta["seed"] = args.seed
+    meta["config_name"] = osp.basename(args.config)
+    meta["work_dir"] = osp.basename(cfg.work_dir.rstrip("/\\"))
 
-    model = build_model(
-        cfg.model, train_cfg=cfg.train_cfg, test_cfg=cfg.test_cfg)
+    model = build_model(cfg.model, train_cfg=cfg.train_cfg, test_cfg=cfg.test_cfg)
 
     if cfg.omnisource:
         # If omnisource flag is set, cfg.data.train should be a list
@@ -152,9 +155,11 @@ def main():
         # we recommend you to use `--validate`
         assert not cfg.omnisource
         if args.validate:
-            warnings.warn('val workflow is duplicated with `--validate`, '
-                          'it is recommended to use `--validate`. see '
-                          'https://github.com/open-mmlab/mmaction2/pull/123')
+            warnings.warn(
+                "val workflow is duplicated with `--validate`, "
+                "it is recommended to use `--validate`. see "
+                "https://github.com/open-mmlab/mmaction2/pull/123"
+            )
         val_dataset = copy.deepcopy(cfg.data.val)
         datasets.append(build_dataset(val_dataset))
     if cfg.checkpoint_config is not None:
@@ -162,7 +167,8 @@ def main():
         # checkpoints as meta data
         cfg.checkpoint_config.meta = dict(
             mmaction_version=__version__ + get_git_hash(digits=7),
-            config=cfg.text)
+            config=cfg.pretty_text,
+        )
 
     train_model(
         model,
@@ -171,8 +177,9 @@ def main():
         distributed=distributed,
         validate=args.validate,
         timestamp=timestamp,
-        meta=meta)
+        meta=meta,
+    )
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
