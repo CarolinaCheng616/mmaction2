437f95283a2df320e59d8ffab6f654f977048dcd
CarolinaCheng616 <948005247@qq.com>
2021-05-08 22:40:29 +0800

    save backbone

--- tools/save_visual_backbone.py
+++ tools/save_visual_backbone.py
@@ -7,25 +7,26 @@ def parse_args():
     parser.add_argument("--in_file", help="input checkpoint filename")
     parser.add_argument("--out_file", help="output checkpoint filename")
     parser.add_argument("--prefix", help="prefix to find")
+    parser.add_argument("--replace", type=str, required=True)
     args = parser.parse_args()
     return args
 
 
-def process_checkpoint(in_file, out_file, prefix):
+def process_checkpoint(in_file, out_file, prefix, replace):
     checkpoint = torch.load(in_file, map_location="cpu")["state_dict"]
 
     backbone = {}
 
     for key in checkpoint:
         if prefix in key:
-            backbone[key.replace(prefix, "backbone")] = checkpoint[key]
+            backbone[key.replace(prefix, replace)] = checkpoint[key]
 
     torch.save(backbone, out_file)
 
 
 def main():
     args = parse_args()
-    process_checkpoint(args.in_file, args.out_file, args.prefix)
+    process_checkpoint(args.in_file, args.out_file, args.prefix, args.replace)
 
 
 if __name__ == "__main__":
