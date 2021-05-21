c3cb31afe1d2b512a7136a8978e0de94eb1a00be
CarolinaCheng616 <948005247@qq.com>
2021-05-21 11:32:30 +0800

    debug

--- configs/MM21/downstream/tsn_clip_r50_1x1x8_50e.py
+++ configs/MM21/downstream/tsn_clip_r50_1x1x8_50e.py
@@ -5,7 +5,7 @@ model = dict(
         type="CLIP", pretrained="RN50", freeze=False, fp16_enabled=True
     ),  # output: [batch * segs, 512]
     cls_head=dict(
-        type="CLIPHead",
+        type="CLIPHeadTmp",
         num_classes=240,
         in_channels=512,
         consensus=dict(type="AvgConsensus", dim=1),
