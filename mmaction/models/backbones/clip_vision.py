6fd01362db656ae8fc4f9ac2da0ece9bfd601392
CarolinaCheng616 <948005247@qq.com>
2021-06-14 19:41:19 +0800

    dataset

--- mmaction/models/backbones/clip_vision.py
+++ mmaction/models/backbones/clip_vision.py
@@ -83,5 +83,5 @@ class CLIPViT(nn.Module):
 
             features = self.model.ln_post(x[:, 0, :])
         # x.shape = [batch * seg, 768]
-        print("\n", flush=True)
+        print("batch", flush=True)
         return features
