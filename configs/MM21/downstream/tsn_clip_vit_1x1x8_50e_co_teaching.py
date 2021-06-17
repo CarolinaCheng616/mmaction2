3361438305ab9157b587341c1e56d5faa1c53e9c
CarolinaCheng616 <948005247@qq.com>
2021-06-17 23:16:20 +0800

    co teaching

--- configs/MM21/downstream/tsn_clip_vit_1x1x8_50e_co_teaching.py
+++ configs/MM21/downstream/tsn_clip_vit_1x1x8_50e_co_teaching.py
@@ -78,7 +78,7 @@ val_pipeline = [
     dict(type="Flip", flip_ratio=0),
     dict(type="Normalize", **img_norm_cfg),
     dict(type="FormatShape", input_format="NCHW"),
-    dict(type="Collect", keys=["imgs", "label"], meta_keys=["idx"], meta_name="idx"),
+    dict(type="Collect", keys=["imgs", "label"], meta_keys=[]),
     dict(type="ToTensor", keys=["imgs"]),
 ]
 test_pipeline = [
@@ -92,7 +92,7 @@ test_pipeline = [
     dict(type="Flip", flip_ratio=0),
     dict(type="Normalize", **img_norm_cfg),
     dict(type="FormatShape", input_format="NCHW"),
-    dict(type="Collect", keys=["imgs", "label"], meta_keys=["idx"], meta_name="idx"),
+    dict(type="Collect", keys=["imgs", "label"], meta_keys=[]),
     dict(type="ToTensor", keys=["imgs"]),
 ]
 data = dict(
