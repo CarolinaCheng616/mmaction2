60b47228ba70afb45d91679f5fb15af5e3c975a3
CarolinaCheng616 <948005247@qq.com>
2021-07-02 16:23:26 +0800

    test

--- configs/MM21/downstream/tsn_clip_vit_1x1x8_50e_hybrid_sample.py
+++ configs/MM21/downstream/tsn_clip_vit_1x1x8_50e_hybrid_sample.py
@@ -23,9 +23,7 @@ test_cfg = dict(average_clips=None)
 dataset_type = "TwoVideoDataset"
 data_root = "data/mm21"
 data_root_val = "data/mm21"
-ann_file_train = (
-    "data/mm21/train_anno data/mm21/imagenet_distill_hard_0.95_resample_filte"
-)
+ann_file_train = "data/mm21/train_anno data/mm21/imagenet_distill_hard_0.95_resample"
 ann_file_val = "data/mm21/val_anno"
 ann_file_test = "data/mm21/val_anno"
 mc_cfg = dict(
